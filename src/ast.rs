03ba01b4e38af4f5b2296a4b45ed243e2c011717
jumbatm <30644300+jumbatm@users.noreply.github.com>
2020-11-18 02:24:12 +1000

    Parse var declarations

--- src/ast.rs
+++ src/ast.rs
@@ -10,5 +10,11 @@ pub enum BinOp {
 pub enum Expression {
     Number(i64),
     BinaryExpression(BinOp, Box<Expression>, Box<Expression>),
+    Ident(String),
     Input,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Statement {
+    VarDecl(Vec<String>), // TODO: Intern strings?
 }
--- src/tip_parser/peg.rs
+++ src/tip_parser/peg.rs
@@ -1,12 +1,23 @@
-
-use crate::ast::{BinOp, Expression};
+use crate::ast::{BinOp, Expression, Statement};
 use peg;
+
 peg::parser! {
     /// PEG grammar for TIP. Note that we wrap this parser in the `TipParser` struct. Most rules are `pub`
     /// to make them testable.
     grammar tip_parser() for str {
 
-        rule _() = [' ' | '\n' | '\t' ]*
+        rule ws() = [' ' | '\n' | '\t']+
+        rule _() = ws()*
+
+        rule statement_contents() -> Statement
+            = "var" ws() first:ident() rest:("," _ id:ident() { id })* {
+                let mut result = vec![first];
+                result.extend(rest);
+                Statement::VarDecl(result)
+            }
+
+        pub rule statement() -> Statement
+            = s:statement_contents() ";" { s }
 
         pub rule expression() -> Expression
             = precedence! {
@@ -21,10 +32,14 @@ peg::parser! {
             }
         pub rule atom() -> Expression
             = number()
+            / id:ident() { Expression::Ident(id) }
             / "(" e:expression() ")" { e }
 
         pub rule number() -> Expression
             = n:$(['0'..='9']+) { Expression::Number(n.parse().unwrap()) }
+
+        pub rule ident() -> String
+            = id:$(['A'..='Z' | 'a'..='z']['A'..='Z' | 'a'..='z' | '0'..='9' | '_' ]*) { id.into() }
     }
 }
 
@@ -92,6 +107,50 @@ mod tests {
                     Box::new(Expression::Number(2)),
                     Box::new(Expression::Number(3)),
                 )),
+            ))
+        );
+    }
+    #[test]
+    fn test_parse_ident() {
+        assert_eq!(tip_parser::ident("x"), Ok("x".to_string()));
+        assert_eq!(tip_parser::ident("y"), Ok("y".to_string()));
+        assert_eq!(tip_parser::ident("z"), Ok("z".to_string()));
+        assert_eq!(tip_parser::ident("xyz"), Ok("xyz".to_string()));
+        assert_eq!(tip_parser::ident("abc123"), Ok("abc123".to_string()));
+        assert_eq!(tip_parser::ident("abc_123"), Ok("abc_123".to_string()));
+    }
+
+    #[test]
+    fn test_parse_var_decl() {
+        assert_eq!(
+            tip_parser::statement("var x;"),
+            Ok(Statement::VarDecl(vec!["x".to_string()]))
+        );
+        assert_eq!(
+            tip_parser::statement("var x, y, z;"),
+            Ok(Statement::VarDecl(
+                vec!["x", "y", "z"]
+                    .into_iter()
+                    .map(|s| s.to_string())
+                    .collect()
+            ))
+        );
+        assert_eq!(
+            tip_parser::statement("var x,y,z;"),
+            Ok(Statement::VarDecl(
+                vec!["x", "y", "z"]
+                    .into_iter()
+                    .map(|s| s.to_string())
+                    .collect()
+            ))
+        );
+        assert_eq!(
+            tip_parser::statement("var a_complex_name,y, result123;"),
+            Ok(Statement::VarDecl(
+                vec!["a_complex_name", "y", "result123"]
+                    .into_iter()
+                    .map(|s| s.to_string())
+                    .collect()
             ))
         );
     }
