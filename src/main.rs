c1158192300727af293a37e9c8586044b04975d3
jumbatm <30644300+jumbatm@users.noreply.github.com>
2020-11-19 21:21:48 +1000

    Add option to parse file & dump ast.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use std::path::PathBuf;
+use structopt::StructOpt;
+use tip::tip_parser;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "tip")]
+struct Opt {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+    #[structopt(short, long)]
+    dump_ast: bool,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    if opt.dump_ast {
+        let src = opt
+            .files
+            .into_iter()
+            .fold(String::new(), |mut acc, current| {
+                let src = std::fs::read_to_string(current).unwrap();
+                acc.push('\n');
+                acc.push_str(&src);
+                acc
+            });
+        dbg!(&src);
+        let ast = tip_parser::parse(src);
+        println!("{:#?}", ast);
+    }
+}
--- src/tip_parser.rs
+++ src/tip_parser.rs
@@ -1,6 +1,6 @@
 mod grammar;
 use crate::ast::Program;
 
-pub fn parse(src: &str) -> Result<Program, peg::error::ParseError<peg::str::LineCol>> {
-    grammar::tip_parser::program(src)
+pub fn parse(src: String) -> Result<Program, peg::error::ParseError<peg::str::LineCol>> {
+    grammar::tip_parser::program(&src)
 }
