29ae88fa78528dede85c85ada9ed3d26dfd85528
jumbatm <30644300+jumbatm@users.noreply.github.com>
2020-12-06 13:09:21 +1000

    Add skeleton for CFG building

--- src/ast.rs
+++ src/ast.rs
@@ -30,7 +30,7 @@ pub enum Expression {
     UnaryExpression(UnOp, Box<Expression>),
     Alloc(Box<Expression>),
     Record(Vec<(Ident, Expression)>),
-    Projection(Box<Expression>, Vec<Ident>)
+    Projection(Box<Expression>, Vec<Ident>),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -64,4 +64,34 @@ pub struct Function {
 #[derive(Debug, PartialEq, Eq)]
 pub struct Program {
     pub functions: Vec<Function>,
+}
+
+pub trait ASTVisitor {
+    fn visit_program(&mut self, p: Program) {
+        super_visit_program(self, p)
+    }
+    fn visit_function(&mut self, f: Function) {
+        super_visit_function(self, f)
+    }
+
+    fn visit_statement_list(&mut self, s: StatementList) {
+        super_visit_statement_list(self, s)
+    }
+    fn visit_statement(&mut self, s: Statement);
+}
+
+pub fn super_visit_program(vis: &mut (impl ASTVisitor + ?Sized), p: Program) {
+    for f in p.functions {
+        vis.visit_function(f)
+    }
+}
+
+pub fn super_visit_function(vis: &mut (impl ASTVisitor + ?Sized), f: Function) {
+    vis.visit_statement_list(f.body)
+}
+
+pub fn super_visit_statement_list(vis: &mut (impl ASTVisitor + ?Sized), sl: StatementList) {
+    for s in sl {
+        vis.visit_statement(s)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod ast;
+pub mod cfg;
 pub mod tip_parser;
